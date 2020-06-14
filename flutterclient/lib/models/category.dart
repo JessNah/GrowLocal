796a4cc544439b93ec00d754f332763982ed83d2
rajeshwarg <rajaeshwar@gmail.com>
2020-06-14 23:19:53 +1000

    Analytics Screen

--- flutterclient/lib/models/category.dart
+++ flutterclient/lib/models/category.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 
 import 'deals.dart';
 
@@ -9,8 +8,9 @@ class Category{
   String id;
   int c;
   List<Deals> d;
+  List<Deals> r;
 
-  Category({this.imageUrl, this.categoryName, this.id, this.c, this.d});
+  Category({this.imageUrl, this.categoryName, this.id, this.c, this.d, this.r});
 }
 
 List<Category> categories = [
@@ -20,6 +20,26 @@ List<Category> categories = [
     id:"1",
     c: 0xFF42A5F5,
     d: [
+      Deals(
+        dealData: "Get 2x rewards on oil by 30/Jun/2020",
+      ),
+      Deals(
+        dealData: "Get 1.5x rewards on pasta by 30/Jun/2020",
+      ),
+      Deals(
+        dealData: "Get 1.5 rewards on rice by 30/Jun/2020",
+      ),
+      Deals(
+        dealData: "Get 1.2x rewards on toor dal by 30/Jun/2020",
+      ),
+      Deals(
+        dealData: "Get 5x rewards on paneer by 30/Jun/2020",
+      ),
+      Deals(
+        dealData: "Get 5% off on all flour by 30/Jun/2020",
+      )
+    ],
+    r: [
       Deals(
         dealData: "Get 2x rewards on oil by 30/Jun/2020",
       ),
@@ -64,6 +84,26 @@ List<Category> categories = [
         Deals(
           dealData: "Get 5% off on all flour by 30/Jun/2020",
         )
+      ],
+      r: [
+        Deals(
+          dealData: "Get 2x rewards on oil by 30/Jun/2020",
+        ),
+        Deals(
+          dealData: "Get 1.5x rewards on pasta by 30/Jun/2020",
+        ),
+        Deals(
+          dealData: "Get 1.5 rewards on rice by 30/Jun/2020",
+        ),
+        Deals(
+          dealData: "Get 1.2x rewards on toor dal by 30/Jun/2020",
+        ),
+        Deals(
+          dealData: "Get 5x rewards on paneer by 30/Jun/2020",
+        ),
+        Deals(
+          dealData: "Get 5% off on all flour by 30/Jun/2020",
+        )
       ]
   ),
   Category(
@@ -90,7 +130,27 @@ List<Category> categories = [
   Deals(
     dealData: "Get 5% off on all flour by 30/Jun/2020",
   )
-]
+],
+      r: [
+        Deals(
+          dealData: "Get 2x rewards on oil by 30/Jun/2020",
+        ),
+        Deals(
+          dealData: "Get 1.5x rewards on pasta by 30/Jun/2020",
+        ),
+        Deals(
+          dealData: "Get 1.5 rewards on rice by 30/Jun/2020",
+        ),
+        Deals(
+          dealData: "Get 1.2x rewards on toor dal by 30/Jun/2020",
+        ),
+        Deals(
+          dealData: "Get 5x rewards on paneer by 30/Jun/2020",
+        ),
+        Deals(
+          dealData: "Get 5% off on all flour by 30/Jun/2020",
+        )
+      ]
   ),
   Category(
       imageUrl: "images/restaurantProblem.jpg",
@@ -98,6 +158,26 @@ List<Category> categories = [
       id:"4",
       c: 0xFF42A5F5,
       d: [
+        Deals(
+          dealData: "Get 2x rewards on oil by 30/Jun/2020",
+        ),
+        Deals(
+          dealData: "Get 1.5x rewards on pasta by 30/Jun/2020",
+        ),
+        Deals(
+          dealData: "Get 1.5 rewards on rice by 30/Jun/2020",
+        ),
+        Deals(
+          dealData: "Get 1.2x rewards on toor dal by 30/Jun/2020",
+        ),
+        Deals(
+          dealData: "Get 5x rewards on paneer by 30/Jun/2020",
+        ),
+        Deals(
+          dealData: "Get 5% off on all flour by 30/Jun/2020",
+        )
+      ],
+      r: [
         Deals(
           dealData: "Get 2x rewards on oil by 30/Jun/2020",
         ),
@@ -142,6 +222,26 @@ List<Category> categories = [
         Deals(
           dealData: "Get 5% off on all flour by 30/Jun/2020",
         )
+      ],
+      r: [
+        Deals(
+          dealData: "Get 2x rewards on oil by 30/Jun/2020",
+        ),
+        Deals(
+          dealData: "Get 1.5x rewards on pasta by 30/Jun/2020",
+        ),
+        Deals(
+          dealData: "Get 1.5 rewards on rice by 30/Jun/2020",
+        ),
+        Deals(
+          dealData: "Get 1.2x rewards on toor dal by 30/Jun/2020",
+        ),
+        Deals(
+          dealData: "Get 5x rewards on paneer by 30/Jun/2020",
+        ),
+        Deals(
+          dealData: "Get 5% off on all flour by 30/Jun/2020",
+        )
       ]
   ),
   Category(
@@ -150,6 +250,26 @@ List<Category> categories = [
       id:"6",
       c: 0xFF42A5F5,
       d: [
+        Deals(
+          dealData: "Get 2x rewards on oil by 30/Jun/2020",
+        ),
+        Deals(
+          dealData: "Get 1.5x rewards on pasta by 30/Jun/2020",
+        ),
+        Deals(
+          dealData: "Get 1.5 rewards on rice by 30/Jun/2020",
+        ),
+        Deals(
+          dealData: "Get 1.2x rewards on toor dal by 30/Jun/2020",
+        ),
+        Deals(
+          dealData: "Get 5x rewards on paneer by 30/Jun/2020",
+        ),
+        Deals(
+          dealData: "Get 5% off on all flour by 30/Jun/2020",
+        )
+      ],
+      r: [
         Deals(
           dealData: "Get 2x rewards on oil by 30/Jun/2020",
         ),
